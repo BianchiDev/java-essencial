e4d678bcb390a342967d8605884bc8fc6e37c8cd
BianchiDev <ifpebianchi@gmail.com>
2023-11-07 17:31:21 -0300

    Modify pakeg

--- src/fundamentos/Programa01.java
+++ src/fundamentos/Programa01.java
@@ -2,10 +2,10 @@ package fundamentos;
 
 public class Programa01 {
 	public static void main(String[] args) {
-	// Declaração de variáveis	
+		// Declaração de variáveis
 		int nu1, nu2, nu3;
-		
-	// Declarando uma variável 
+
+		// Declarando uma variável
 		int valor;
 	}
 
